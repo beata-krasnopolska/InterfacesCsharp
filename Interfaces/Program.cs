82574fab824cac3fdf9c6b4cdbb7e4708ba41d7e
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-07-18 18:41:18 +0200

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transakcje t1 = new Transakcje("01", "25/03/2011", 331);
+            Transakcje t2 = new Transakcje("01", "11/02/2015", 3321);
+            t1.WyswietlDane();
+            t2.WyswietlDane();
+            Console.ReadKey();
+        }
+        public interface ITransakcje
+        {
+            void WyswietlDane();
+            int PoliczIlosc();
+        }
+        public class Transakcje : ITransakcje
+        {
+            private string kod;
+            private string data;
+            private int ilosc;
+
+            public Transakcje()
+            {
+                kod = string.Empty;
+                data = string.Empty;
+                ilosc = 0;
+            }
+            public Transakcje(string kodIn, string dataIn, int iloscIn)
+            {
+                kod = kodIn;
+                data = dataIn;
+                ilosc = iloscIn;
+            }
+            public int PoliczIlosc()
+            {
+                return ilosc;
+            }
+            public void WyswietlDane()
+            {
+                Console.WriteLine("Kod: {0}", kod);
+                Console.WriteLine("Data: {0}", data);
+                Console.WriteLine("Ilość: {0}", ilosc);
+            }
+        }
+    }
+}
